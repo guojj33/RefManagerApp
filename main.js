0a244fed8f220683fcbb7e5e13ac382c80900952
guojj33 <guojj33@mail2.sysu.edu.cn>
2022-05-20 22:55:01 +0800

    update

--- main.js
+++ main.js
@@ -5,32 +5,49 @@ const spawn = require("child_process").spawn;
 const expressPath = './ExpressApp/server.js'
 const nodePath = './ExpressApp/node-v16.15.0-win-x64/node.exe'
 
-app.on('ready', function() {
-  console.log('ready')
-  let nodeProc = spawn(nodePath, [expressPath], {cwd: app.getAppPath()})
-  nodeProc.stdout.on('data', data => {
-    console.log(data.toString())
-  })
-  nodeProc.stderr.on('data', data => {
-    console.log('stderr:', data.toString())
-  })
-  nodeProc.on('close', (code, signal) => {
-    console.log('node close')
-  })
-  mainWindow = new BrowserWindow({
-    show: false,
-    maximizable: true,
-    autoHideMenuBar: false,
-    // useContentSize: true,
-    resizable: true,
-  });
-  mainWindow.on("close", () => {
-    console.log('mainWindow close')
-    nodeProc.kill()
-  });
+const gotTheLock = app.requestSingleInstanceLock()
 
-  mainWindow.loadURL(__dirname + '/ExpressApp/index.html');
-  mainWindow.maximize()
-  mainWindow.show()
-  mainWindow.focus();
-})
+let mainWindow = undefined
+
+if (!gotTheLock) {
+  app.quit()
+}
+else {
+  app.on('second-instance', (eve, cmd, wd) => {
+    if (mainWindow != undefined) {
+      if (mainWindow.isMinimized())
+        mainWindow.restore()
+      mainWindow.show()
+      mainWindow.focus()
+    }
+  })
+  app.on('ready', function() {
+    console.log('ready')
+    let nodeProc = spawn(nodePath, [expressPath], {cwd: app.getAppPath()})
+    nodeProc.stdout.on('data', data => {
+      console.log(data.toString())
+    })
+    nodeProc.stderr.on('data', data => {
+      console.log('stderr:', data.toString())
+    })
+    nodeProc.on('close', (code, signal) => {
+      console.log('node close')
+    })
+    mainWindow = new BrowserWindow({
+      show: false,
+      maximizable: true,
+      autoHideMenuBar: false,
+      // useContentSize: true,
+      resizable: true,
+    });
+    mainWindow.on("close", () => {
+      console.log('mainWindow close')
+      nodeProc.kill()
+    });
+  
+    mainWindow.loadURL(__dirname + '/ExpressApp/index.html');
+    mainWindow.maximize()
+    mainWindow.show()
+    mainWindow.focus()
+  })
+}
